ee1a7924c5be661d686f7d679c55b24f1ea5dadd
Sergey Medvedev <medsv@yandex.ru>
2026-01-17 12:30:52 +0300

    Закомментировал температуру

--- assistant.py
+++ assistant.py
@@ -14,6 +14,7 @@ class Assistant:
         res = self.client.responses.create(
             model = self.model,
             reasoning = { "effort" : "low" },
+#            temperature = 0.2,
             store = True,
             previous_response_id = previous_response_id,
             instructions = self.instructions,
